c369bddf938738d43fabfd7e8b8dab8addf0be77
Malaya2184 <malayakumarswain8@gmail.com>
2024-03-20 20:01:23 +0530

    dummy methods created in controller for API end points

--- src/main/java/com/spider/productservice/controllers/ProductController.java
+++ src/main/java/com/spider/productservice/controllers/ProductController.java
@@ -1,9 +1,41 @@
 package com.spider.productservice.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spider.productservice.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id){
+        return new Product();
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return List.of(new Product());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return new Product();
+    }
+
+    @PatchMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product){
+        return new Product();
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable Long id, @RequestBody Product product){
+        return new Product();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id){
+
+    }
 }
