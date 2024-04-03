d638dc7670af095ff9ead858973960bf2cc4b6ad
Malaya2184 <malayakumarswain8@gmail.com>
2024-04-03 12:00:41 +0530

    data base connect sucessful

--- src/main/java/com/spider/productservice/models/Category.java
+++ src/main/java/com/spider/productservice/models/Category.java
@@ -1,5 +1,7 @@
 package com.spider.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,9 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Category {
+    @Id
     private long id;
     private String title;
 }
--- src/main/java/com/spider/productservice/models/Product.java
+++ src/main/java/com/spider/productservice/models/Product.java
@@ -1,18 +1,24 @@
 package com.spider.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Product {
-
+    @Id
     private long id;
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
     private String description;
     private String image;
