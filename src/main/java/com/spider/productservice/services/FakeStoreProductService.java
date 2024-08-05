75566c4c1b390a07159f0c9fc2bcbcb74be961a2
Malaya2184 <malayakumarswain8@gmail.com>
2024-08-06 03:16:50 +0530

    added redis for aching and data cached for fakes store product get by id

--- src/main/java/com/spider/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/spider/productservice/services/FakeStoreProductService.java
@@ -3,7 +3,7 @@ package com.spider.productservice.services;
 import com.spider.productservice.dtos.FakeStoreProductDto;
 import com.spider.productservice.models.FakeStoreCategory;
 import com.spider.productservice.models.FakeStoreProduct;
-import com.spider.productservice.models.FakeStoreProduct;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +13,18 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service("fakeStoreProductService") // explicitly named this bin so that can get this exact bean using @Qualifier anotation
-public class FakeStoreProductService implements ProductService<FakeStoreProduct>{
+@Service("fakeStoreProductService")
+// explicitly named this bin so that can get this exact bean using @Qualifier anotation
+public class FakeStoreProductService implements ProductService<FakeStoreProduct> {
     private final RestTemplate restTemplate;
+    private final RedisTemplate redisTemplate;
 
-    public FakeStoreProductService(RestTemplate restTemplate) {
+    public FakeStoreProductService(RestTemplate restTemplate, RedisTemplate redisTemplate) {
         this.restTemplate = restTemplate;
+        this.redisTemplate = redisTemplate;
     }
 
-    private FakeStoreProduct convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto){
+    private FakeStoreProduct convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
 
         FakeStoreProduct product = new FakeStoreProduct();
         product.setId(fakeStoreProductDto.getId());
@@ -35,6 +38,7 @@ public class FakeStoreProductService implements ProductService<FakeStoreProduct>
         product.setFakeStoreCategory(category);
         return product;
     }
+
     private FakeStoreProductDto convertProductToFakeStoreDto(FakeStoreProduct product) {
         FakeStoreProductDto fakeStoreProductDto = new FakeStoreProductDto();
         fakeStoreProductDto.setTitle(product.getTitle());
@@ -45,13 +49,24 @@ public class FakeStoreProductService implements ProductService<FakeStoreProduct>
         fakeStoreProductDto.setDescription(product.getDescription());
         return fakeStoreProductDto;
     }
+
     @Override
     public FakeStoreProduct getProductByid(Long id) {
+
+//        First search in the redis if cachehit then return the object or if cache miss stor thatt obj in  redis
+        FakeStoreProduct fakeStoreProduct = (FakeStoreProduct) redisTemplate.opsForHash().get("FAKE_STORE_PRODUCT", "FAKE_STORE_PRODUCT_" + id);
+        if (fakeStoreProduct != null) {
+//            cache hit
+            return fakeStoreProduct;
+        }
+//        FakeStoreProduct fakeStoreProduct = redisTemplate.opsForHash().get();
         //        call the FakeStore API here to get the product with given ID here.
-        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products/"+id, FakeStoreProductDto.class);
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
 //        convert DTO to actual FakeStoreProduct
         if (fakeStoreProductDto != null) {
-            return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+            FakeStoreProduct product = convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+            redisTemplate.opsForHash().put("FAKE_STORE_PRODUCT", "FAKE_STORE_PRODUCT_" + id, product);
+            return product;
         }
         return null;
     }
